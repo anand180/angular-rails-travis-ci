9c95a936f59c185b5d86938c9e233b975663974e
Anand Dhillon <anand180@gmail.com>
2013-08-27 12:25:15 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularRailsTravisCi::Application.config.secret_key_base = '9197d271bd6ef8c273982df550357f1b1cc20fc69bfe58444417d478d5007f576379f86370711e6397fc6d4e6baa29dc4512df7732ac2a5f2fa6a9646f129f22'

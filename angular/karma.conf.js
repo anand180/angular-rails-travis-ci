3e68047fe3987d92feebb88b7a4256e5b6f2592e
Anand Dhillon <anand180@gmail.com>
2013-08-28 12:34:22 -0400

    Working karma runner

--- angular/karma.conf.js
+++ angular/karma.conf.js
@@ -1,61 +1,76 @@
 // Karma configuration
+// Generated on Wed Aug 28 2013 12:22:27 GMT-0400 (EDT)
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function(config) {
+  config.set({
+    
+    preprocessors: {
+          '**/*.coffee': ['coffee']
+        },
 
-// list of files / patterns to load in the browser
-files = [
-    JASMINE,
-    JASMINE_ADAPTER,
-    'app/components/jquery/jquery.js',
-    'app/components/angular/angular.js',
-    'app/components/angular-mocks/angular-mocks.js',
-    'app/scripts/**/*.js',
-    'app/scripts/**/*.coffee',
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-    'test/mock/**/*.js',
-    'test/spec/**/*.js',
-//    'app/scripts/**/*.coffee',
-    'test/mock/**/*.coffee',
-    'test/spec/**/*.coffee'
-];
 
-// list of files to exclude
-exclude = [];
+    // frameworks to use
+    frameworks: ['jasmine'],
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
 
-// web server port
-port = 8080;
+    // list of files / patterns to load in the browser
+    files: [
+      'app/components/jquery/jquery.js',
+      'app/components/angular/angular.js',
+      'app/components/angular-mocks/angular-mocks.js',
+      'app/scripts/**/*.coffee',
+      'test/spec/**/*.coffee'
+    ],
 
-// cli runner port
-runnerPort = 9100;
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // list of files to exclude
+    exclude: [
+      
+    ],
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 15000;
+    // web server port
+    port: 9876,
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
